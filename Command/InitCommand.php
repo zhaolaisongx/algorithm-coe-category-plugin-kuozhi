dc7352cabb5c4d04effa715a5af49b6cea9fb599
zhaolaisong <zhaolaisong@edusoho.com>
2019-07-03 10:36:08 +0800

    增加初始化指令

---
+++ Command/InitCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CategoryTestPlugin\Command;
+
+use AppBundle\Command\BaseCommand;
+use Biz\Taxonomy\Service\CategoryService;
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        $this->setName('project_plan:init');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>开始初始化系统</info>');
+
+        $this->initCategory($output);
+
+        $output->writeln('<info>初始化系统完毕</info>');
+    }
+
+    protected function initCategory($output) {
+
+        $output->write('  初始化分类分组');
+
+        $testGroup = $this->getCategoryService()->getGroupByCode('test');
+
+        if (!$testGroup) {
+            $testGroup = $this->getCategoryService()->addGroup(array(
+                'name' => '分类嵌套测试',
+                'code' => 'test',
+                'depth' => 5,
+            ));
+        }
+
+        $output->writeln(' ...<info>成功</info>');
+    }
+
+    /**
+     * @return CategoryService
+     */
+    protected function getCategoryService()
+    {
+        return ServiceKernel::instance()->getBiz()->service('Taxonomy:CategoryService');
+    }
+
+    protected function getCategoryTestService()
+    {
+        return ServiceKernel::instance()->getBiz()->service('CategoryTestPlugin:CategoryTestService');
+    }
+
+}
--- Scripts/InstallScript.php
+++ Scripts/InstallScript.php
@@ -7,13 +7,25 @@ class InstallScript extends BaseInstallScript
     public function install()
     {
         $connection = $this->getConnection();
-        /* create you database table
-        $connection->exec("DROP TABLE IF EXISTS `cash_orders_log`;");
+        /* create you database table 还需插入category_group 一条数据，再默认插入一条数据才能启动 应该用command*/
         $connection->exec("
-            CREATE TABLE IF NOT EXISTS `cash_orders_log` (
-            `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-            PRIMARY KEY (`id`)
-          ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-        ");*/
+            CREATE TABLE IF NOT EXISTS `category_test` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+              `code` varchar(64) NOT NULL DEFAULT '' COMMENT '分类编码',
+              `name` varchar(255) NOT NULL COMMENT '分类名称',
+              `icon` varchar(255) NOT NULL DEFAULT '' COMMENT '图标',
+              `path` varchar(255) NOT NULL DEFAULT '' COMMENT '分类完整路径',
+              `weight` int(11) NOT NULL DEFAULT '0' COMMENT '分类权重',
+              `groupId` int(10) unsigned NOT NULL COMMENT '分类组ID',
+              `parentId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父分类ID',
+              `orgId` int(10) unsigned NOT NULL DEFAULT '1' COMMENT '组织机构ID',
+              `orgCode` varchar(255) NOT NULL DEFAULT '1.' COMMENT '组织机构内部编码',
+              `description` text,
+              `leftNum` int(11) NOT NULL DEFAULT '0' COMMENT '左边点',
+              `rightNum` int(11) NOT NULL DEFAULT '0' COMMENT '右边点',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `uri` (`code`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+        ");
     }
 }
